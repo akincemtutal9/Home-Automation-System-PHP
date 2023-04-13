4c88217540dae969e77adece1234a75fa46a2e6c
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-04-13 16:21:51 +0300

    register password validation & login form

---
+++ js/registerpasswordvalidation.js
@@ -0,0 +1,16 @@
+function validateForm() {
+    var password = document.forms["register_form"]["password"].value;
+    var cpassword = document.forms["register_form"]["cpassword"].value;
+    if (password != cpassword) {
+        alert("Passwords do not match");
+        return false;
+    }
+    if(password.length < 8){
+        alert("Password must be at least 8 characters long");
+        return false;
+    }
+    return true;
+
+    
+
+}
