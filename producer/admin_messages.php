1870204b91172d3276946ccce98956aae6cf89e0
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-05-30 17:25:03 +0300

    feat: admin pages & include dropdown

--- producer/admin_messages.php
+++ producer/admin_messages.php
@@ -1,6 +1,7 @@
 <?php
 include '../php/session_admin.php';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,12 +17,10 @@ include '../php/session_admin.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="../css/adminstyle.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel='stylesheet' type='text/css' media='screen' href='../css/user-account.css'>
     <script src="../js/deleteuser.js"></script>
     
     
-    <title>Account</title>
+    <title>Admin Dashboard</title>
 </head>
 
 <body>
@@ -35,41 +34,34 @@ include '../php/session_admin.php';
             <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
                 <div class="d-flex align-items-center">
                     <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-                    <h2 class="fs-2 m-0 text-dark">Account</h2>
+                    <h2 class="fs-2 m-0">Messages</h2>
                 </div>
-                
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                 <!--ADMIN dropdown-menu -->
-                 <?php include '../producer/admin_dropdown.php'?>
+                <!--ADMIN dropdown-menu -->
+                <?php include '../producer/admin_dropdown.php'?>
             </nav>
 
-            <div class="user-page">
-                <div class="sidebar-user">
-                    <div class="user-photo">
-                        <img src="../images/profile.png" alt="user image">
-                        <h3>John Doe</h3>
-                    </div>
-                    <div class="actions">
-                        <ul>
-                            <li><a href="profil.php" class="chosen"> My Account</a></li>
-                            <li><a href="profiledit.html" class="not-chosen">Edit Account</a></li>
-                            <li><a href="profilsupport.html" class="not-chosen">Support</a></li>
-                            <li><a href="profilpassword.html" class="not-chosen">Change Password</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="user-info-board">
-                    <h1>My Account</h1>
-                    <div class="user-infos">
-                        <div class="">
-                        <?php
-                            include '../php/user_profil.php';
-                        ?>
-                            
-                        </div>
+            <div class="container-fluid px-4">
+                <div class="row my-5">
+                    <h3 class="fs-4 mb-3">Messages</h3>
+                    <div class="col">
+                        <table class="table bg-white rounded shadow-sm  table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">id</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Message</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- MESAJLARI BURAYA GİRCEN LİST MESAJ FALAN YAP -->
+                                <p>BURAYA MESAJLARI GİRCEN</p>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
