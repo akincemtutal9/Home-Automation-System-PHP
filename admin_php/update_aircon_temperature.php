6409035ca50212274547faf36f6dadaa71d9f25c
Burak Sazlı <buraksazli0@gmail.com>
2023-06-05 22:29:39 +0300

    air conditioner effect for room temperature added

--- admin_php/update_aircon_temperature.php
+++ admin_php/update_aircon_temperature.php
@@ -1,28 +1,27 @@
 <?php
-require_once "../database/config.php"; // Config dosyanızın yolunu doğru şekilde ayarlayın
+require_once "../database/config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Parametreleri al
     $deviceID = $_POST['deviceID'];
     $temperature = $_POST['temperatureAir'];
-
-    // SQL sorgusu
+    $sql12 = "SELECT isOpen, mode FROM air_conditioner WHERE deviceID='$deviceID'";
+    mysqli_query($conn, $sql12);
+    $result12 = mysqli_query($conn, $sql12);
+    if (mysqli_num_rows($result12) > 0){
+        $row12 = mysqli_fetch_assoc($result12);
+    }
+    if($row12['isOpen'] == 1){
+        $sql23  = "UPDATE device JOIN room ON device.roomID = room.roomID SET room.temperature = '$temperature' WHERE device.deviceID = '$deviceID'";
+        mysqli_query($conn, $sql23);
+    }
     $sql = "UPDATE air_conditioner SET temperature = $temperature WHERE deviceID = $deviceID";
 
-    // Bağlantıyı kapatmadan önce bağlantıyı kontrol edin
-    if (!$conn) {
-        die("Veritabanı bağlantısı başarısız: " . mysqli_connect_error());
-    }
-
-    // Sorguyu çalıştır
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($conn, $sql) ) {
         header("Location: " . $_SERVER["HTTP_REFERER"]); 
         exit();
     } else {
         echo "Güncelleme hatası: " . mysqli_error($conn);
     }
-
-    // Veritabanı bağlantısını kapat
     mysqli_close($conn);
 }
 ?>
