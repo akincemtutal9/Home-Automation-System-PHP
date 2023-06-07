e482cb0dd7368567fd919d0100f4158c906a6139
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-06-07 04:24:04 +0300

    feat: Charts for admin

--- admin_php/delete_aircond.php
+++ admin_php/delete_aircond.php
@@ -4,21 +4,28 @@ include '../database/config.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $airID = $_POST['airID'];
 
-    // Insert device into device table
-    $airSql = "DELETE FROM air_conditioner WHERE deviceID = $airID";
+    // Delete from statistics table
+    $statisticsSql = "DELETE FROM statistics WHERE deviceID = $airID";
 
-    if ($conn->query($airSql) === TRUE) {
-        // Insert corresponding entry into light table
-        $deviceSql = "DELETE FROM device WHERE deviceID = $airID";
+    if ($conn->query($statisticsSql) === TRUE) {
+        // Delete from air_conditioner table
+        $airSql = "DELETE FROM air_conditioner WHERE deviceID = $airID";
 
-        if ($conn->query($deviceSql) === TRUE) {
-            header("Location: " . $_SERVER["HTTP_REFERER"]); 
-            exit();
+        if ($conn->query($airSql) === TRUE) {
+            // Delete from device table
+            $deviceSql = "DELETE FROM device WHERE deviceID = $airID";
+
+            if ($conn->query($deviceSql) === TRUE) {
+                header("Location: " . $_SERVER["HTTP_REFERER"]); 
+                exit();
+            } else {
+                echo "Error deleting from device table: " . $conn->error;
+            }
         } else {
-            echo "Error inserting into light table: " . $conn->error;
+            echo "Error deleting from air_conditioner table: " . $conn->error;
         }
     } else {
-        echo "Error inserting into device table: " . $conn->error;
+        echo "Error deleting from statistics table: " . $conn->error;
     }
 
     $conn->close();    
--- producer/admin_page.php
+++ producer/admin_page.php
@@ -1,5 +1,142 @@
 <?php
 include '../php/session_admin.php';
+include '../database/config.php';
+
+
+$totalRoomsCount = 0;
+$usedDevicesCount = 0;
+$totalUsersCount = 0;
+
+$airCount = 0;
+$dishwasherCount = 0;
+$electricBlanketCount = 0;
+$fanCount = 0;
+$lightCount = 0;
+$robotToyCount = 0;
+$robotVacuumCount = 0;
+$washingMachineCount = 0;
+
+
+// Count of Used Devices
+$usedDevicesSql = 'SELECT COUNT(*) as count FROM device';
+$usedDevicesResult = $conn->query($usedDevicesSql);
+if ($usedDevicesResult && $usedDevicesResult->num_rows > 0) {
+    $usedDevicesRow = $usedDevicesResult->fetch_assoc();
+    $usedDevicesCount = $usedDevicesRow['count'];
+}
+
+// Total Users Count
+$totalUsersSql = "SELECT COUNT(*) as count FROM user WHERE user_type = 'user' ";
+$totalUsersResult = $conn->query($totalUsersSql);
+if ($totalUsersResult && $totalUsersResult->num_rows > 0) {
+    $totalUsersRow = $totalUsersResult->fetch_assoc();
+    $totalUsersCount = $totalUsersRow['count'];
+}
+
+// Total Rooms Count
+$totalRoomsSql = 'SELECT COUNT(*) as count FROM room';
+$totalRoomsResult = $conn->query($totalRoomsSql);
+if ($totalRoomsResult && $totalRoomsResult->num_rows > 0) {
+    $totalRoomsRow = $totalRoomsResult->fetch_assoc();
+    $totalRoomsCount = $totalRoomsRow['count'];
+}
+
+
+// Air Conditioner Count
+$airSql = 'SELECT COUNT(*) as count FROM air_conditioner';
+$airResult = $conn->query($airSql);
+if ($airResult && $airResult->num_rows > 0) {
+    $airRow = $airResult->fetch_assoc();
+    $airCount = $airRow['count'];
+}
+
+// Dishwasher Count
+$dishwasherSql = 'SELECT COUNT(*) as count FROM dishwasher';
+$dishwasherResult = $conn->query($dishwasherSql);
+if ($dishwasherResult && $dishwasherResult->num_rows > 0) {
+    $dishwasherRow = $dishwasherResult->fetch_assoc();
+    $dishwasherCount = $dishwasherRow['count'];
+}
+
+// Electric Blanket Count
+$electricBlanketSql = 'SELECT COUNT(*) as count FROM electric_blanket';
+$electricBlanketResult = $conn->query($electricBlanketSql);
+if ($electricBlanketResult && $electricBlanketResult->num_rows > 0) {
+    $electricBlanketRow = $electricBlanketResult->fetch_assoc();
+    $electricBlanketCount = $electricBlanketRow['count'];
+}
+
+// Fan Count
+$fanSql = 'SELECT COUNT(*) as count FROM fan';
+$fanResult = $conn->query($fanSql);
+if ($fanResult && $fanResult->num_rows > 0) {
+    $fanRow = $fanResult->fetch_assoc();
+    $fanCount = $fanRow['count'];
+}
+
+// Light Count
+$lightSql = 'SELECT COUNT(*) as count FROM light';
+$lightResult = $conn->query($lightSql);
+if ($lightResult && $lightResult->num_rows > 0) {
+    $lightRow = $lightResult->fetch_assoc();
+    $lightCount = $lightRow['count'];
+}
+
+// Robot Toy Count
+$robotToySql = 'SELECT COUNT(*) as count FROM robot_toy';
+$robotToyResult = $conn->query($robotToySql);
+if ($robotToyResult && $robotToyResult->num_rows > 0) {
+    $robotToyRow = $robotToyResult->fetch_assoc();
+    $robotToyCount = $robotToyRow['count'];
+}
+
+// Robot Vacuum Cleaner Count
+$robotVacuumSql = 'SELECT COUNT(*) as count FROM robot_vacum_cleaner';
+$robotVacuumResult = $conn->query($robotVacuumSql);
+if ($robotVacuumResult && $robotVacuumResult->num_rows > 0) {
+    $robotVacuumRow = $robotVacuumResult->fetch_assoc();
+    $robotVacuumCount = $robotVacuumRow['count'];
+}
+
+// Washing Machine Count
+$washingMachineSql = 'SELECT COUNT(*) as count FROM washing_machine';
+$washingMachineResult = $conn->query($washingMachineSql);
+if ($washingMachineResult && $washingMachineResult->num_rows > 0) {
+    $washingMachineRow = $washingMachineResult->fetch_assoc();
+    $washingMachineCount = $washingMachineRow['count'];
+}
+
+// Sayıları kullanarak Chart 1'e aktarabilirsiniz
+echo "
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var ctx1 = document.getElementById('myChart1').getContext('2d');
+            var myChart1 = new Chart(ctx1, {
+                type: 'line',
+                data: {
+                    labels: ['Air Conditioner', 'Dishwasher', 'Electric Blanket', 'Fan', 'Light', 'Robot Toy', 'Robot Vacuum Cleaner', 'Washing Machine'],
+                    datasets: [{
+                        label: 'TOTAL DEVICES',
+                        data: [$airCount, $dishwasherCount, $electricBlanketCount, $fanCount, $lightCount, $robotToyCount, $robotVacuumCount, $washingMachineCount],
+                        backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                        borderColor: 'rgba(0, 123, 255, 1)',
+                        borderWidth: 2,
+                        pointBackgroundColor: 'rgba(0, 123, 255, 1)',
+                        pointRadius: 4
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+    </script>
+";
 ?>
 
 <!DOCTYPE html>
@@ -38,7 +175,7 @@ include '../php/session_admin.php';
                 </button>
 
 
-                 <?php include '../producer/admin_dropdown.php'?>
+                <?php include '../producer/admin_dropdown.php' ?>
             </nav>
 
             <div class="container-fluid px-4">
@@ -46,8 +183,8 @@ include '../php/session_admin.php';
                     <div class="col-md-3">
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2">1</h3>
-                                <p class="fs-5">Home Automation</p>
+                                <h3 class="fs-2"><?php echo $totalUsersCount?></h3>
+                                <p class="fs-5">Total Users</p>
                             </div>
                             <i class="fas fa-car fs-1 primary-text border rounded-full secondary-bg p-3"></i>
                         </div>
@@ -56,7 +193,7 @@ include '../php/session_admin.php';
                     <div class="col-md-3">
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2">49</h3>
+                                <h3 class="fs-2"><?php echo $usedDevicesCount?></h3>
                                 <p class="fs-5">Sales</p>
                             </div>
                             <i class="fas fa-hand-holding-usd fs-1 primary-text border rounded-full secondary-bg p-3"></i>
@@ -66,7 +203,7 @@ include '../php/session_admin.php';
                     <div class="col-md-3">
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2">38</h3>
+                                <h3 class="fs-2"><?php echo $totalRoomsCount?></h3>
                                 <p class="fs-5">Delivery</p>
                             </div>
                             <i class="fas fa-truck fs-1 primary-text border rounded-full secondary-bg p-3"></i>
@@ -84,59 +221,90 @@ include '../php/session_admin.php';
                     </div>
                 </div>
 
-                <div class="row my-5">
-                    <h3 class="fs-4 mb-3">Recent Requests</h3>
-                    <div class="col">
-                        <table class="table bg-white rounded shadow-sm  table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col" width="50">#</th>
-                                    <th scope="col">Request</th>
-                                    <th scope="col">Consumer</th>
-                                    <th scope="col">Accept or Reject</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Light</td>
-                                    <td>Akin</td>
-                                    <td>
-                                        <button class="btn btn-primary mt-auto">Accept</button>
-                                        <button class="btn btn-danger mt-auto">Reject</button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Air Conditioner</td>
-                                    <td>Cem</td>
-                                    <td>
-                                        <button class="btn btn-primary mt-auto">Accept</button>
-                                        <button class="btn btn-danger mt-auto">Reject</button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Vacuum Cleaner</td>
-                                    <td>Burak</td>
-                                    <td>
-                                        <button class="btn btn-primary mt-auto">Accept</button>
-                                        <button class="btn btn-danger mt-auto">Reject</button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th>
-                                    <td>Washing Machine</td>
-                                    <td>AkinCem</td>
-                                    <td>
-                                        <button class="btn btn-primary mt-auto">Accept</button>
-                                        <button class="btn btn-danger mt-auto">Reject</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+
+                <!-- ... -->
+                <div class="container-fluid px-4">
+                    <!-- ... -->
+
+                    <div class="row g-3 my-2">
+                        <!-- ... -->
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="p-3 bg-white shadow-sm rounded">
+                                <canvas id="myChart1"></canvas>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="p-3 bg-white shadow-sm rounded">
+                                <canvas id="myChart2"></canvas>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script>
+                    // document.addEventListener('DOMContentLoaded', function() {
+                    //     var ctx1 = document.getElementById('myChart1').getContext('2d');
+                    //     var myChart1 = new Chart(ctx1, {
+                    //         type: 'line',
+                    //         data: {
+                    //             labels: ['Air Conditioner', 'Dishwasher', 'Electric Blanket', 'Fan', 'Light', 'Robot Toy', 'Robot Vacuum Cleaner', 'Washing Machine'],
+                    //             datasets: [{
+                    //                 label: 'Device Count',
+                    //                 data: [12
+                                    
+                                    
+                    //                 , 19, 3, 5, 2, 3, 3, 3],
+                    //                 backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                    //                 borderColor: 'rgba(0, 123, 255, 1)',
+                    //                 borderWidth: 2,
+                    //                 pointBackgroundColor: 'rgba(0, 123, 255, 1)',
+                    //                 pointRadius: 4
+                    //             }]
+                    //         },
+                    //         options: {
+                    //             responsive: true,
+                    //             scales: {
+                    //                 y: {
+                    //                     beginAtZero: true
+                    //                 }
+                    //             }
+                    //         }
+                    //     });
+
+                        var ctx2 = document.getElementById('myChart2').getContext('2d');
+                        var myChart2 = new Chart(ctx2, {
+                            type: 'bar',
+                            data: {
+                                labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
+                                datasets: [{
+                                    label: 'Sales',
+                                    data: [50, 30, 20, 45, 60],
+                                    backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                                    borderColor: 'rgba(0, 123, 255, 1)',
+                                    borderWidth: 2,
+                                    hoverBackgroundColor: 'rgba(0, 123, 255, 0.8)',
+                                    hoverBorderColor: 'rgba(0, 123, 255, 1)',
+                                }]
+                            },
+                            options: {
+                                responsive: true,
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        });
+                   // });
+                </script>
+                <!-- ... -->
+
+
 
             </div>
         </div>
