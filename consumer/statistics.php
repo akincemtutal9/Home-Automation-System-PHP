e3c920b663ae4f9836a8803b11e08a7408fd6da1
Burak Sazlı <buraksazli0@gmail.com>
2023-06-18 19:41:27 +0300

    statistics title changed to activities

--- consumer/statistics.php
+++ consumer/statistics.php
@@ -33,7 +33,7 @@ include '../php/session_user.php';
             <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
                 <div class="d-flex align-items-center">
                     <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-                    <h2 class="fs-2 m-0 text-dark">Statistics</h2>
+                    <h2 class="fs-2 m-0 text-dark">Activities</h2>
                 </div>
                 
                 <?php include 'navbar.php';?>
